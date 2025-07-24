6f3a8a00bb6ca7cc2b90cbcc0466da8d513a44cb
gl1tchxd-git <contact@gl1tchxd.at>
2025-07-24 14:28:50 +0200

    added some gnome extensions

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -3,6 +3,12 @@
   environment.systemPackages = with pkgs.gnomeExtensions; [
     dash-to-dock
     unite
+    arcmenu
+    blur-my-shell
+    forge
+    just-perfection
+    open-bar
+    system-monitor
   ];
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
