c37afe1ae07b5b6a4332ffb17a1b6d5f42c893b7
gl1tchxd-git <contact@gl1tchxd.at>
2025-10-14 14:08:02 +0200

    fixed bug wher unstable.<pkg> doesnt return the unstable branch version in home-manager module

--- hosts/laptop-felix/configuration.nix
+++ hosts/laptop-felix/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running 'nixos-help').
 
-{ config, pkgs, unstable, inputs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
   imports =
@@ -144,7 +144,6 @@
     isNormalUser = true;
     description = "Felix Buchsteiner";
     extraGroups = [ "networkmanager" "wheel" "libvirtd" "dialout" "docker" ];
-    # User packages moved to home-manager modules
   };
 
   hardware.opentabletdriver.enable = true;
