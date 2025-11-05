1d1c3f527f68e7ce21272d2d0a8605f39fc063dc
gl1tchxd-git <contact@gl1tchxd.at>
2025-11-05 12:56:05 +0100

    switched from home-manager to nixos for package pritunl-client

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -7,4 +7,20 @@
     ];
   };
   systemd.services.ModemManager.enable = false;
+
+  # pritunl client for VPN connections
+
+  systemd.services.pritunl-client = {
+    enable = true;
+    description = "Pritunl Client Service";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      ExecStart = "${pkgs.pritunl-client}/bin/pritunl-client-service";
+      Restart = "always";
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    unstable.pritunl-client
+  ];
 }
