a43a50584658351f82189664e506d4b376657d5a
gl1tchxd-git <contact@gl1tchxd.at>
2025-11-06 08:04:30 +0100

    added xorg X11 libraries for GUI development

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -2,4 +2,12 @@
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   services.xserver.desktopManager.gnome.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    xorg.libX11
+    xorg.libXext
+    xorg.libXrender
+    xorg.libXtst
+    xorg.libXi
+  ];
 }
