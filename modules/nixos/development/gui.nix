725bd688cd8ec4043f5453c647562644f1028062
gl1tchxd-git <contact@gl1tchxd.at>
2025-11-20 08:36:29 +0100

    added libX libraries to PATH

--- modules/nixos/development/gui.nix
+++ modules/nixos/development/gui.nix
@@ -1,5 +1,10 @@
 { config, pkgs, lib, ... }: {
   environment.systemPackages = with pkgs; [
+    freetype
+    fontconfig
+    xorg.libXrender
+    xorg.libXinerama
+    xorg.libXext
     xorg.libXrandr
     xorg.libX11
     xorg.libXcursor
@@ -12,4 +17,25 @@
     libGL
     glfw-wayland
   ];
+
+  environment.variables = {
+    LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [
+      pkgs.freetype
+      pkgs.fontconfig
+      pkgs.xorg.libXrender
+      pkgs.xorg.libXinerama
+      pkgs.xorg.libXext
+      pkgs.xorg.libXrandr
+      pkgs.xorg.libX11
+      pkgs.xorg.libXcursor
+      pkgs.xorg.libXi
+      pkgs.xorg.libXxf86vm
+      pkgs.libGL
+      pkgs.glfw
+      pkgs.wayland
+      pkgs.libxkbcommon
+      pkgs.libGL
+      pkgs.glfw-wayland
+    ]}";
+  };
 }
