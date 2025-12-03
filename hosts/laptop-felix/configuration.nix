9a66695095913c1684d54f341066e064802b71ed
gl1tchxd-git <contact@gl1tchxd.at>
2025-12-03 20:08:37 +0100

    changed gui.nix to java.nix and removed gui libraries and instead added all important nix-ld libraries

--- hosts/laptop-felix/configuration.nix
+++ hosts/laptop-felix/configuration.nix
@@ -11,7 +11,7 @@ let
     /desktop/gnome.nix
     /development/embedded.nix
     /development/sysadmin.nix
-    /development/gui.nix
+    /development/java.nix
     /development/langs.nix
     /gaming/steam.nix
     /hardware/audio.nix
