e289e98146a76bb65e42cc23a4b3be869547831d
gl1tchxd-git <contact@gl1tchxd.at>
2026-02-25 18:43:29 +0100

    fixed issue where cosmic wasnt launching after grub

--- modules/nixos/hardware/graphics.nix
+++ modules/nixos/hardware/graphics.nix
@@ -10,11 +10,4 @@
 
   
   services.xserver.videoDrivers = [ "intel" ];
-
-    # Enable VA-API acceleration for video
-  environment.variables = {
-    LIBVA_DRIVER_NAME = "iHD";
-    WAYLAND_DISPLAY = "wayland-0";
-    XDG_RUNTIME_DIR = "/run/user/1000";
-  };
 }
