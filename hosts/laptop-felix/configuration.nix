1d84ba66e2a466826505694247ab1e345e912efa
gl1tchxd-git <contact@gl1tchxd.at>
2026-03-02 10:14:21 +0100

    switched back to gnome for now

--- hosts/laptop-felix/configuration.nix
+++ hosts/laptop-felix/configuration.nix
@@ -8,8 +8,8 @@ let
   modules = map (m: ../../modules/nixos + m) [
     /boot/grub.nix
     /boot/plymouth.nix
-    # /desktop/gnome.nix
-    /desktop/cosmic.nix
+    /desktop/gnome.nix
+    # /desktop/cosmic.nix
     /development/embedded.nix
     /development/sysadmin.nix
     /development/jetbrains.nix
