b34bfb36347207837318b3447c306104b4e2875b
gl1tchxd-git <contact@gl1tchxd.at>
2026-03-10 10:26:13 +0100

    set autoStart for VPNS to false updated vpn config

--- modules/nixos/networking/default.nix
+++ modules/nixos/networking/default.nix
@@ -1,10 +1,6 @@
-{ pkgs, lib, ... }: {
-  networking.networkmanager = {
-    enable = true;
-    plugins = with pkgs; [
-      networkmanager-openvpn
-    ];
-  };
+{ pkgs, lib, ... }:
+{
+  networking.networkmanager.enable = true;
   systemd.services.ModemManager.enable = false;
 
   services.avahi.enable = true;
--- modules/nixos/networking/vpns.nix
+++ modules/nixos/networking/vpns.nix
@@ -1,27 +1,7 @@
-{ config, pkgs, inputs, ... }:
+{ pkgs, ... }:
 
 {
-  services.openvpn.servers = {
-    Slovenia = {
-        config = ''
-          config /home/felix/nixos/data/config/Slovenia.conf
-          auth-user-pass /home/felix/nixos/data/config/.creds
-        '';
-      autoStart = false;
-    };
-    US = {
-      config = ''
-        config /home/felix/nixos/data/config/US.conf
-        auth-user-pass /home/felix/nixos/data/config/.creds
-      '';
-      autoStart = false;
-    };
-    Austria = {
-      config = ''
-        config /home/felix/nixos/data/config/Austria.conf
-        auth-user-pass /home/felix/nixos/data/config/.creds
-      '';
-      autoStart = true;
-    };
-  };
+  environment.systemPackages = with pkgs; [
+    wireguard-tools
+  ];
 }
