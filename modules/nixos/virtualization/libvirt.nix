292583281ab3025fd6fe2780f1048309537b7156
gl1tchxd-git <contact@gl1tchxd.at>
2026-03-11 19:11:43 +0100

    fixed path issues with ovmf

--- modules/nixos/virtualization/libvirt.nix
+++ modules/nixos/virtualization/libvirt.nix
@@ -11,4 +11,15 @@
   environment.systemPackages = with pkgs; [
       virt-manager
   ];
+
+  system.activationScripts.libvirtFirmwareLinks = {
+    text = ''
+      mkdir -p /var/lib/libvirt/firmware
+      ln -sf ${pkgs.qemu_kvm}/share/qemu/edk2-x86_64-secure-code.fd \
+        /var/lib/libvirt/firmware/edk2-x86_64-secure-code.fd
+      ln -sf ${pkgs.qemu_kvm}/share/qemu/edk2-i386-vars.fd \
+        /var/lib/libvirt/firmware/edk2-i386-vars.fd
+    '';
+    deps = [];
+  };
 }
