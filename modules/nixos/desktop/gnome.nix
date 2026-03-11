7cb34b5c61884bf1e6702ac1528b99d54941d06b
gl1tchxd-git <contact@gl1tchxd.at>
2026-03-11 10:12:35 +0100

    formatted gnome

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -1,5 +1,11 @@
-{ config, pkgs, lib, ... }: {
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.desktopManager.gnome.enable = true;
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+{
+  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.wayland = true;
+  services.desktopManager.gnome.enable = true;
 }
