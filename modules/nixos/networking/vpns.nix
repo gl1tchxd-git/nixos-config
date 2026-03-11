6162df3461bb4875c7b6ca751472daea7a826df4
gl1tchxd-git <contact@gl1tchxd.at>
2026-03-11 10:39:19 +0100

    fix gitignore and untrack vpn configs

--- modules/nixos/networking/vpns.nix
+++ modules/nixos/networking/vpns.nix
@@ -8,6 +8,30 @@
   networking.networkmanager.ensureProfiles = {
     environmentFiles = [ "/home/felix/nixos/data/config/.wg-env" ];
     profiles = {
+      "Slovenia" = {
+        connection = {
+          id = "Slovenia";
+          type = "wireguard";
+          interface-name = "wg-slovenia";
+          autoconnect = "false";
+        };
+        wireguard = {
+          private-key = "$WG_PRIVATE_KEY";
+        };
+        "wireguard-peer.yPdmxOfzm06fotkt/dlaAiyxWPaWfCuDPaUljNx+c38=" = {
+          endpoint = "si-lju.prod.surfshark.com:51820";
+          allowed-ips = "0.0.0.0/0;";
+        };
+        ipv4 = {
+          method = "manual";
+          address1 = "10.14.0.2/16";
+          dns = "162.252.172.57;149.154.159.92;";
+        };
+        ipv6 = {
+          method = "disabled";
+        };
+      };
+
       "US" = {
         connection = {
           id = "US";
@@ -20,6 +44,30 @@
         };
         "wireguard-peer.1g84fGxVJokKXdMYEJKjN6/opyYN/YSHmrMyw0v6VnM=" = {
           endpoint = "us-hou.prod.surfshark.com:51820";
+          allowed-ips = "0.0.0.0/0;";
+        };
+        ipv4 = {
+          method = "manual";
+          address1 = "10.14.0.2/16";
+          dns = "162.252.172.57;149.154.159.92;";
+        };
+        ipv6 = {
+          method = "disabled";
+        };
+      };
+
+      "Austria" = {
+        connection = {
+          id = "Austria";
+          type = "wireguard";
+          interface-name = "wg-austria";
+          autoconnect = "false";
+        };
+        wireguard = {
+          private-key = "$WG_PRIVATE_KEY";
+        };
+        "wireguard-peer.dPZe8Jq3Hu0k07MDk+Y4+AS2XHSLYalyg91TSFXRYEA=" = {
+          endpoint = "at-vie.prod.surfshark.com:51820";
           allowed-ips = "0.0.0.0/0;";
         };
         ipv4 = {
