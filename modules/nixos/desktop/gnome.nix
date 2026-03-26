d4b72c06fb53a627ecd6ddf4e371494cab9f88d2
gl1tchxd-git <contact@gl1tchxd.at>
2026-03-26 08:07:43 +0100

    fixed gnome rdp

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -9,5 +9,7 @@
   services.displayManager.gdm.wayland = true;
   services.desktopManager.gnome.enable = true;
 
-  services.gnome.gnome-remote-desktop.enable = true;
+  systemd.services.gnome-remote-desktop = {
+    wantedBy = [ "graphical.target" ];
+  };
 }
