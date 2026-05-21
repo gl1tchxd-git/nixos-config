5a334cb4725fbba10b9c05a3342f7460d5e49e0f
gl1tchxd-git <contact@gl1tchxd.at>
2026-05-21 08:09:47 +0200

    added 32bit support again.... and env vars for steam

--- modules/nixos/hardware/graphics.nix
+++ modules/nixos/hardware/graphics.nix
@@ -1,6 +1,8 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+{
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
       mesa
       intel-media-driver
@@ -8,6 +10,9 @@
     ];
   };
 
-  
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+  };
+
   services.xserver.videoDrivers = [ "intel" ];
 }
