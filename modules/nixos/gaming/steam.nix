0e1eb79444fdd61c632d19f69ea2d7124ed56e30
gl1tchxd-git <contact@gl1tchxd.at>
2026-05-21 08:12:26 +0200

    added steam x11 override

--- modules/nixos/gaming/steam.nix
+++ modules/nixos/gaming/steam.nix
@@ -1,9 +1,17 @@
-{ pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+{
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+
+    package = pkgs.steam.override {
+      extraEnv = {
+        GDK_BACKEND = "x11";
+      };
+    };
   };
+
   hardware.steam-hardware.enable = true;
 }
