53f9468e93062974a58c838806de4c6d085ced17
gl1tchxd-git <contact@gl1tchxd.at>
2026-05-21 07:52:08 +0200

    added 32bit graphics support

--- hosts/desktop-felix/configuration.nix
+++ hosts/desktop-felix/configuration.nix
@@ -52,7 +52,10 @@
   };
   services.desktopManager.plasma6.enable = true;
 
-  hardware.graphics.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     open = true;
